1be6384049df186db447024b611747461a1586b7
Dmitry Samsonau <demn_@tut.by>
2014-07-13 21:31:34 +0300

    fizzbuzz and logicaloperations done

--- app/flowControl.js
+++ app/flowControl.js
@@ -1,16 +1,26 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
+  function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+  
   return {
     fizzBuzz : function(num) {
-      // write a function that receives a number as its argument;
-      // if the number is divisible by 3, the function should return 'fizz';
-      // if the number is divisible by 5, the function should return 'buzz';
-      // if the number is divisible by 3 and 5, the function should return
-      // 'fizzbuzz';
-      //
-      // otherwise the function should return the number, or false if no number
-      // was provided or the value provided is not a number
+      if (!isNumber(num)) {
+        return false;
+      }
+      if (!(num % 15)) {
+        return 'fizzbuzz';
+      }
+      if (!(num % 5)) {
+        return 'buzz';
+      }
+      if (!(num % 3)) {
+        return 'fizz';
+      }
+
+      return num;
     }
   };
 });
