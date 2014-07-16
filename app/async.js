4d3d25810d11070f4d5178dd62c28a35f26d3e4a
Dmitry Samsonau <demn_@tut.by>
2014-07-16 19:50:31 +0300

    Done with all; some tests have been improved, some functions will not pass new requirements

--- app/async.js
+++ app/async.js
@@ -2,33 +2,35 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define([ 'jquery' ], function($) {
   return {
-    async : function(value) {
+      async: function (value) {
+          return new Promise(function myfunction(resolve) {
+              resolve(value);
+          });
+      },
 
-    },
+      manipulateRemoteData: function (url) {
+          return new Promise(function (resolve) {
+              var arr = [];
+              var req = new XMLHttpRequest();
+              req.open('GET', url);
 
-    manipulateRemoteData: function (url) {
-        return new Promise(function (resolve) {
-            var arr = [];
-            var req = new XMLHttpRequest();
-            req.open('GET', url);
+              req.onload = function () {
+                  if (req.status === 200) {
+                      var response = req.response;
+                      var obj = JSON.parse(response);
 
-            req.onload = function () {
-                if (req.status === 200) {
-                    var response = req.response;
-                    var obj = JSON.parse(response);
+                      for (var i = 0; i < obj.people.length; i++) {
+                          arr.push(obj.people[i].name);
+                      }
+                      arr.sort();
 
-                    for (var i = 0; i < obj.people.length; i++) {
-                        arr.push(obj.people[i].name);
-                    }
-                    arr.sort();
+                      resolve(arr);
+                  }
+              };
 
-                    resolve(arr);
-                }
-            };
-
-            req.send();
-        });
-    },
+              req.send();
+          });
+      },
 
   };
 });
