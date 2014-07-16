49c0394142c8940778b3ed20d388a4f0ff60e328
Dmitry Samsonau <demn_@tut.by>
2014-07-16 18:39:52 +0300

    49/51; implemented counter

--- app/count.js
+++ app/count.js
@@ -2,7 +2,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function () {
   return {
-    count : function (start, end) {
+      count: function (start, end) {
+          var nIntervId = setInterval(flash, 100);
+
+          function flash() {
+              console.log(start++);
+              if (start > end) clearInterval(nIntervId);
+          }
+
+          return {
+              cancel: function () {
+                  clearInterval(nIntervId);
+              }
+          };
     }
   };
 });
